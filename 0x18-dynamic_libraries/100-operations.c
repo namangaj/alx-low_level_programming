b4093833d88d1546bcf4f5270266d7369fd6e979
Joshua Namanga <namangaj@gmail.com>
2023-12-11 12:42:58 -0800

    This is my 0x18-dynamic_libraries project

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+* add - add two intergers
+* @a: first int
+* @b: second int
+* Return: addition
+*/
+int add(int a, int b)
+{
+        return (a + b);
+}
+/**
+* sub - subtract two intergers
+* @a: first int
+* @b: second int
+* Return: subtracted final result
+*/
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+* mul - multiply  two intergers
+* @a: first int
+* @b: second int
+* Return: product of two intergers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* div - divide two intergers
+* @a: first int
+* @b: second int
+* Return: division result
+* */
+int div (int a, int b)
+{
+        return (a / b);
+}
+/**
+* mod - mod of two intergers
+* @a: first int
+* @b: second int
+* Return: final amswer
+* */
+int mod (int a, int b)
+{
+	return (a % b);
+}
